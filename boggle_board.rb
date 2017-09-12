5c1e7eb0036ca7fe9a01e215979a158c1625a854
Petr Fila <pet.fila@gmail.com>
2017-09-12 16:56:03 +1000

    the game randomly prints letters now

--- boggle_board.rb
+++ boggle_board.rb
@@ -3,10 +3,24 @@ class BoggleBoard
     #How to describe it
     #Creating an array of 16 underscores
     @grid = Array.new(16, "_")
-
+    @alphabet = ("A".."Z").to_a
   end
 
   def shake!
+    # #take spaces
+    # output_string = String.new
+    # #REPLACE them with random letters (create a variable for this)
+    # alphabet_to_mess_up = @alphabet
+    # #spit it into our board
+    # 4.times do
+    #   output_string << alphabet_to_mess_up.sort_by {rand}.shift(4).join(" ") + ("\n")
+    # end
+    # #call the shake! method BoggleBoard class
+    #  puts output_string
+    # #could use MAP
+    @grid.map! do |space|
+      space = @alphabet.shuffle.take(1)
+    end
   end
 
   # Defining to_s on an object controls how the object is
@@ -22,7 +36,9 @@ class BoggleBoard
     end
 
     #print the output
-    output_string
+    puts output_string
   end
 end
-puts BoggleBoard.new.to_s
+game = BoggleBoard.new
+# puts game.to_s
+puts game.shake!
